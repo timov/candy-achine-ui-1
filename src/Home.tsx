c4a40bfa8e4beecba03b813d17275fe8e349dbd3
timov <timovmartin@gmail.com>
2022-05-24 02:12:02 +0200

    Add files via upload

--- src/Home.tsx
+++ src/Home.tsx
@@ -588,7 +588,7 @@ const Home = (props: HomeProps) => {
                 <MintContainer>
                     <DesContainer>
                         <NFT elevation={3}>
-                            <h2>My NFT</h2>
+                            <h2>Luna Eyes NFT</h2>
                             <br/>
                             <div><Price
                                 label={isActive && whitelistEnabled && (whitelistTokenBalance > 0) ? (whitelistPrice + " " + priceLabel) : (price + " " + priceLabel)}/><Image
